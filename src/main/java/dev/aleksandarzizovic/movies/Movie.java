87cfae4f51b8170777c2622a06c03778103b6e31
z1z0v1c <aleksandar.zizovic@outlook.com>
2023-01-22 02:00:36 +0100

    Get all movies

--- src/main/java/dev/aleksandarzizovic/movies/Movie.java
+++ src/main/java/dev/aleksandarzizovic/movies/Movie.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collation = "movies")
+@Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,8 +22,9 @@ public class Movie {
     private String releaseDate;
     private String trailerLink;
     private String poster;
-    private List<String> genres;
     private List<String> backdrops;
+    private List<String> genres;
     @DocumentReference
-    private List<Review> reviewIds;
+    private List<Review> reviews;
+
 }
--- src/main/java/dev/aleksandarzizovic/movies/MovieController.java
+++ src/main/java/dev/aleksandarzizovic/movies/MovieController.java
@@ -1,5 +1,6 @@
 package dev.aleksandarzizovic.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> allMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
