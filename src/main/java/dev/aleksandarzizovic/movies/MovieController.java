6d1a4c443dd0d8c91c3f9feb4372a91e0b4c93db
z1z0v1c <aleksandar.zizovic@outlook.com>
2023-01-22 01:26:13 +0100

    Add controller

---
+++ src/main/java/dev/aleksandarzizovic/movies/MovieController.java
@@ -0,0 +1,17 @@
+package dev.aleksandarzizovic.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("All movies", HttpStatus.OK);
+    }
+}
